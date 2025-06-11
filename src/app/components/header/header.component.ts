8797e311d000726c4b0c4ccffbaef8ea1418fd8b
Mebo <samkharadzemerab@gmail.com>
2025-06-11 11:29:15 +0400

    add header with current time

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { DatePipe, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public currentTime!: Date;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.currentTime = new Date();
+      setInterval(() => {
+        this.currentTime = new Date();
+      }, 1000);
+    }
+  }
 }
